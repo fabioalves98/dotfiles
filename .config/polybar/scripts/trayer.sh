0dfc763ab74e21278330436e91d7ababd8d8fa96
Fábio Alves <fabio@FA-Tower.lan>
2021-03-25 20:56:59 +0000

    desktop changes on trayer and tmux

--- .config/polybar/scripts/trayer.sh
+++ .config/polybar/scripts/trayer.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cmd="trayer --SetDockType false --widthtype request --align left --edge right --distancefrom right --distance -8 --margin 40 --transparent true --alpha 255 --iconspacing 3"
+cmd="trayer --SetDockType false --widthtype request --align left --edge right --distancefrom right --distance -8 --margin 40 --transparent true --alpha 255 --iconspacing 3 --monitor 1"
 
 pgrep -x trayer >/dev/null && pkill trayer|| eval "$cmd" &
 
