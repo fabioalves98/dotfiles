de1f7bcd355fceee074601973790945659ca65d6
Fábio <fabiomcalves98@gmail.com>
2021-03-27 18:58:25 +0000

    terminal resizing and floating settings

--- .scripts/terminal.py
+++ .scripts/terminal.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-
-from os import system
+import time
 from sys import argv
 from i3ipc import Connection, Event
 
@@ -10,13 +9,16 @@ i3 = Connection()
 tree = i3.get_tree()
 command = ''
 instance = ''
+resolution = ''
 
 if argv[1] == 'tmux':
     instance = 'floatingTmux'
     command = 'tmux'
+    resolution = '1000px 600px'
 elif argv[1] == 'ranger':
     instance = 'floatingRanger'
     command = 'ranger'
+    resolution = '1280px 720px'
 
 create=True
 # See if exsists
@@ -25,7 +27,11 @@ for container in tree:
         create=False
 
 if create:
-    i3.command('exec urxvt -name ' + instance + ' -e bash -i -c "' + command + '" ')
+    i3.command(f'exec --no-startup-id alacritty --class {instance} -e zsh -i -c "{command}"')
+    time.sleep(0.1)
+    i3.command(f'[instance="{instance}"] resize set {resolution}')
+    i3.command(f'[instance="{instance}"] move position center')
+
 
 show=True
 # See if shows or hides
@@ -33,9 +39,8 @@ focused = tree.find_focused().workspace()
 for container in focused:
     if container.window_instance == instance:
         show=False
-        # i3.command('[instance=\"' + instance + '\"] move container to workspace 11:Terminal')
-        i3.command('[instance=\"' + instance + '\"] move scratchpad')
+        i3.command(f'[instance="{instance}"] move scratchpad')
 
 if show:
-    i3.command('[instance=\"' + instance + '\"] move container to workspace current')
-    i3.command('[instance=\"' + instance + '\"] focus')
+    i3.command(f'[instance="{instance}"] move container to workspace current')
+    i3.command(f'[instance="{instance}"] focus')
